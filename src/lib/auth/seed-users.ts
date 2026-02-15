98a8709f02a68eadaa1cb4b48010b8b146041524
Aarifshaik <skaarif.10@gmail.com>
2026-02-15 10:55:45 +0530

    Changed Names

--- src/lib/auth/seed-users.ts
+++ src/lib/auth/seed-users.ts
@@ -2,17 +2,17 @@ import type { User } from "@/types/domain"
 
 export const SEEDED_USERS: User[] = [
   {
-    id: "operator-1",
+    id: "Mr. Baji Baba",
     name: "Operator 1",
     pinHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
   },
   {
-    id: "operator-2",
+    id: "Mr. Laxman Reddy",
     name: "Operator 2",
     pinHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
   },
   {
-    id: "operator-3",
+    id: "Mr. Venkat Reddy",
     name: "Operator 3",
     pinHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
   },
